d65fc7fefa049b6a0e5cc5cf24e8a1e89d2e6290
Karan Sanjeev <yo@alphaman.me>
2023-10-11 19:20:35 +0530

    user middleware and test axios

---
+++ common/middlewares/isUser.tsx
@@ -0,0 +1,34 @@
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/router"
+import { ComponentType, useEffect, useState } from "react"
+import { UserData, Role } from "@/common/types"
+
+const isUser = (ChildComponent: ComponentType, roles: Role[] = []) => () => {
+    const [loading, setLoading] = useState<boolean>(true)
+
+    const router = useRouter()
+    const session = useSession()
+
+    useEffect(() => {
+        if (session.status === 'unauthenticated') {
+            router.push('/auth/login')
+        }
+
+        if (roles.length > 0 && session.status == 'authenticated') {
+            setLoading(false)
+
+            const user = session.data?.user as UserData
+            if (!roles.includes(user.role)) {
+                router.push('/khfkfrkjf')
+            }
+        }
+    }, [session.status])
+
+    if (loading) {
+        return <h1>Loading....</h1>
+    }
+
+    return <ChildComponent />
+}
+
+export default isUser
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,26 @@
-import { Button, Card, Table, TableBody, TableCell, TableHead, TableHeaderCell, TableRow, Title } from "@tremor/react";
-import { Text } from "@tremor/react";
+import { Button, Card, Table, TableBody, TableCell, TableHead, TableHeaderCell, TableRow, Title } from '@tremor/react'
+import { Text } from '@tremor/react'
 import { BuildingStorefrontIcon } from '@heroicons/react/24/outline'
+import isUser from '@/common/middlewares/isUser'
+import { useSession } from 'next-auth/react'
+import useAxios from '@/common/axios'
+import { useEffect } from 'react'
 
 const Home = () => {
+    const session = useSession()
+    const axios = useAxios()
+
+    useEffect(() => {
+        (async () => {
+            const res = await axios.get('/stores?include=profile,profile.state,profile.district')
+            console.log(res.data)
+        })()
+    }, [session.status])
+
     return (
         <main className="p-12">
             <Title>Admin Dashboard</Title>
-            <Text>Welcome to your dashboard, <strong>Karan</strong></Text>
+            <Text>Welcome to your dashboard, <strong>{session.data?.user?.name}</strong></Text>
 
             <Card className="mt-6">
                 <Title>Stores</Title>
@@ -58,4 +72,4 @@ const Home = () => {
     )
 }
 
-export default Home
+export default isUser(Home, ['admin'])
