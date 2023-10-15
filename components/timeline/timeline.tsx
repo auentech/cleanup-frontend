abeb16b59f9cb9bd025d6d7b89db29a7626b988a
Karan Sanjeev <yo@alphaman.me>
2023-10-15 16:40:05 +0530

    changes to timeline

--- components/timeline/timeline.tsx
+++ components/timeline/timeline.tsx
@@ -2,11 +2,12 @@ import { ReactNode } from "react"
 
 type TimelineProps = {
     children: ReactNode
+    className?: string
 }
 
-const Timeline = ({ children }: TimelineProps) => {
+const Timeline = ({ children, className }: TimelineProps) => {
     return (
-        <ol className="relative border-l border-gray-200 dark:border-gray-700">
+        <ol className={"relative border-l border-gray-200 dark:border-gray-700 " + className}>
             {children}
         </ol>
     )
