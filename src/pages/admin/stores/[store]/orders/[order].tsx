85f11c60696cad3e68aa63c4725569c30a072641
Karan Sanjeev <yo@alphaman.me>
2023-10-15 21:05:08 +0530

    fix build

--- src/pages/admin/stores/[store]/orders/[order].tsx
+++ src/pages/admin/stores/[store]/orders/[order].tsx
@@ -66,7 +66,7 @@ const ShowOrder = () => {
                     <Title>{store?.data.name} store</Title>
                     <Badge icon={BuildingStorefrontIcon} size="xs" className="ml-4">{store?.data.code}</Badge>
                 </Flex>
-                <Subtitle>{order?.data.customer?.name}'s order from {order?.data.customer?.profile?.address}</Subtitle>
+                <Subtitle>{order?.data.customer?.name}&lsquo;s order from {order?.data.customer?.profile?.address}</Subtitle>
 
             </div>
 
