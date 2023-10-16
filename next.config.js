828af8d3d2f92834f95c533cbf4b565634f1201d
Karan Sanjeev <yo@alphaman.me>
2023-10-16 13:14:38 +0530

    upgrade deps and use experimental features

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    optimizePackageImports: [
+      "lodash",
+      "@tremor/react",
+      "@uiball/loaders",
+      "@heroicons/react/24/outline",
+    ]
+  }
 }
 
 module.exports = nextConfig
