f3129a4133d02dab7a718888a2c03e0b918299d8
Karan Sanjeev <yo@alphaman.me>
2023-10-18 10:46:37 +0530

    use axios and handle proper backend url

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,6 +3,7 @@ import type { AuthOptions, Session } from 'next-auth'
 import NextAuth from 'next-auth/next'
 import CredentialsProvider from 'next-auth/providers/credentials'
 import { LoginResponse } from '@/common/types'
+import axios, { AxiosError } from 'axios'
 
 const dayjs = Dayjs()
 
@@ -16,29 +17,24 @@ export const authOptions: AuthOptions = {
             },
             authorize: async credentials => {
                 try {
-                    const domain = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth/login`
-                    const response = await fetch(domain, {
-                        method: 'POST',
-                        body: JSON.stringify({
-                            email: credentials?.email,
-                            password: credentials?.password
-                        }),
+                    const response = await axios.post<LoginResponse>('/api/auth/login', {
+                        email: credentials?.email,
+                        password: credentials?.password
+                    }, {
+                        baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
                         headers: {
                             'Content-Type': 'application/json'
                         }
                     })
 
-                    const data: LoginResponse = await response.json()
-                    if (response.ok && data) {
-                        return {
-                            ...data.data,
-                            token: data.meta.token
-                        }
+                    return {
+                        ...response.data.data,
+                        token: response.data.meta.token,
+                        store_id: response.data.meta.store_id
                     }
-
-                    throw new Error('Invalid credentials')
                 } catch (e) {
-                    console.error(e)
+                    const error = e as AxiosError
+                    console.error(error.response?.data)
                     throw new Error('Unable to log you in')
                 }
             },
