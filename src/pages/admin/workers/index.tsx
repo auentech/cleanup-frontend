368dd6155ca5c4333023ef6e27d4cc5d6c5fa0cc
Karan Sanjeev <yo@alphaman.me>
2023-11-05 10:40:15 +0530

    Skeleton for admin workers list

--- src/pages/admin/workers/index.tsx
+++ src/pages/admin/workers/index.tsx
@@ -11,6 +11,7 @@ import { Waveform } from "@uiball/loaders"
 import dynamic from "next/dynamic"
 import { Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure } from "@nextui-org/react"
 import { useRouter } from "next/router"
+import TableSkeleton from "@/components/table-skeleton"
 
 const LazyCreateWorker = dynamic(() => import('@/components/admin/create-worker'), {
     loading: () => (
@@ -82,38 +83,44 @@ const Workers = () => {
                     </TabList>
                     <TabPanels>
                         <TabPanel>
-                            <Table className="mt-4">
-                                <TableHead>
-                                    <TableRow>
-                                        <TableHeaderCell>Name</TableHeaderCell>
-                                        <TableHeaderCell>Role</TableHeaderCell>
-                                        <TableHeaderCell>Email</TableHeaderCell>
-                                        <TableHeaderCell>Phone</TableHeaderCell>
-                                        <TableHeaderCell>State</TableHeaderCell>
-                                        <TableHeaderCell>Action</TableHeaderCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {workers?.data.map(worker => (
-                                        <TableRow key={worker.id}>
-                                            <TableCell>{worker.name}</TableCell>
-                                            <TableCell>
-                                                <Badge style={{ textTransform: 'capitalize' }}>
-                                                    {worker.role}
-                                                </Badge>
-                                            </TableCell>
-                                            <TableCell>{worker.email}</TableCell>
-                                            <TableCell>{worker.phone}</TableCell>
-                                            <TableCell>{worker?.profile?.state.name}</TableCell>
-                                            <TableCell>
-                                                <Button icon={UserIcon} variant="secondary" color="gray" onClick={_ => editWorker(worker)}>
-                                                    Show worker
-                                                </Button>
-                                            </TableCell>
-                                        </TableRow>
-                                    ))}
-                                </TableBody>
-                            </Table>
+                            <div className="mt-4">
+                                {workers == undefined ? (
+                                    <TableSkeleton numCols={6} numRows={5} />
+                                ) : (
+                                    <Table className="mt-4">
+                                        <TableHead>
+                                            <TableRow>
+                                                <TableHeaderCell>Name</TableHeaderCell>
+                                                <TableHeaderCell>Role</TableHeaderCell>
+                                                <TableHeaderCell>Email</TableHeaderCell>
+                                                <TableHeaderCell>Phone</TableHeaderCell>
+                                                <TableHeaderCell>State</TableHeaderCell>
+                                                <TableHeaderCell>Action</TableHeaderCell>
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+                                            {workers.data.map(worker => (
+                                                <TableRow key={worker.id}>
+                                                    <TableCell>{worker.name}</TableCell>
+                                                    <TableCell>
+                                                        <Badge style={{ textTransform: 'capitalize' }}>
+                                                            {worker.role}
+                                                        </Badge>
+                                                    </TableCell>
+                                                    <TableCell>{worker.email}</TableCell>
+                                                    <TableCell>{worker.phone}</TableCell>
+                                                    <TableCell>{worker?.profile?.state.name}</TableCell>
+                                                    <TableCell>
+                                                        <Button icon={UserIcon} variant="secondary" color="gray" onClick={_ => editWorker(worker)}>
+                                                            Show worker
+                                                        </Button>
+                                                    </TableCell>
+                                                </TableRow>
+                                            ))}
+                                        </TableBody>
+                                    </Table>
+                                )}
+                            </div>
                         </TabPanel>
                         <TabPanel>
                             {theIndex == 1 && <LazyCreateWorker />}
