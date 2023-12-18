1bda2b4c6a7f3fb8b4d9ef48be1ec38e56863ace
Karan Sanjeev <yo@alphaman.me>
2023-12-18 20:30:39 +0530

    Add react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,23 @@ import '@/styles/globals.css'
 import { NextUIProvider } from '@nextui-org/react'
 import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-export default function App({ Component, pageProps: { session, ...pageProps }, }: AppProps) {
-  return (
-    <NextUIProvider>
-      <SessionProvider session={session}>
-        <Component {...pageProps} />
-      </SessionProvider>
-    </NextUIProvider>
-  )
+const queryClient = new QueryClient()
+
+export default function App({
+    Component,
+    pageProps: { session, ...pageProps },
+}: AppProps) {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <NextUIProvider>
+                <SessionProvider session={session}>
+                    <Component {...pageProps} />
+                </SessionProvider>
+            </NextUIProvider>
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+    )
 }
