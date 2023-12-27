26b29e08f6de73c3fc6f7303cf6fac37ba51fea4
Karan Sanjeev <yo@alphaman.me>
2023-12-28 01:37:32 +0530

    Allow edit of phone number for store

--- components/admin/edit-store.tsx
+++ components/admin/edit-store.tsx
@@ -1,10 +1,31 @@
-import useAxios from "@/common/axios"
-import { CreateStoreError, StatesResponse, FreeOperatorsResponse, DistrictsResponse, StoreResponse, BackendGeneralResponse } from "@/common/types"
-import { GlobeAsiaAustraliaIcon, ArrowPathIcon } from "@heroicons/react/24/outline"
-import { Text, MultiSelect, MultiSelectItem, TextInput, SearchSelect, SearchSelectItem, Divider, Flex, Button } from "@tremor/react"
+import useAxios from '@/common/axios'
+import {
+    CreateStoreError,
+    StatesResponse,
+    FreeOperatorsResponse,
+    DistrictsResponse,
+    StoreResponse,
+    BackendGeneralResponse,
+} from '@/common/types'
+import {
+    GlobeAsiaAustraliaIcon,
+    ArrowPathIcon,
+} from '@heroicons/react/24/outline'
+import {
+    Text,
+    MultiSelect,
+    MultiSelectItem,
+    TextInput,
+    SearchSelect,
+    SearchSelectItem,
+    Divider,
+    Flex,
+    Button,
+    NumberInput,
+} from '@tremor/react'
 import lodashMap from 'lodash/map'
-import { useRouter } from "next/router"
-import { useState, useEffect } from "react"
+import { useRouter } from 'next/router'
+import { useState, useEffect } from 'react'
 
 const EditStore = () => {
     const axios = useAxios()
@@ -23,6 +44,7 @@ const EditStore = () => {
     const [pincode, setPincode] = useState<string>('')
     const [state, setState] = useState<string>('')
     const [district, setDistrict] = useState<string>('')
+    const [phone, setPhone] = useState<number>(0)
 
     const [states, setStates] = useState<StatesResponse>()
     const [operators, setOperators] = useState<FreeOperatorsResponse>()
@@ -31,16 +53,21 @@ const EditStore = () => {
     useEffect(() => {
         const initOperators = async () => {
             if (store == undefined) return
-            const opsResponse = await axios.get<FreeOperatorsResponse>('/workers/free-operators')
+            const opsResponse = await axios.get<FreeOperatorsResponse>(
+                '/workers/free-operators',
+            )
 
-            const currentOperators = lodashMap(store?.data.operators, ops => ops.user)
+            const currentOperators = lodashMap(
+                store?.data.operators,
+                (ops) => ops.user,
+            )
             if (opsResponse.data.data.length > 0) {
-                opsResponse.data.data.forEach(user => {
+                opsResponse.data.data.forEach((user) => {
                     currentOperators.push(user)
                 })
 
                 setOperators({
-                    data: currentOperators
+                    data: currentOperators,
                 })
 
                 return
@@ -54,27 +81,32 @@ const EditStore = () => {
 
     useEffect(() => {
         if (operators == undefined) return
-        setCurrentStoreOps(lodashMap(store?.data.operators, ops => ops.user_id + ''))
-        setStoreOperators(lodashMap(operators.data, ops => ops.id + ''))
+        setCurrentStoreOps(
+            lodashMap(store?.data.operators, (ops) => ops.user_id + ''),
+        )
+        setStoreOperators(lodashMap(operators.data, (ops) => ops.id + ''))
     }, [operators])
 
     useEffect(() => {
-        (async () => {
+        ;(async () => {
             const response = await axios.get<StatesResponse>('/states/')
             setStates(response.data)
 
-            const storeResponse = await axios.get<StoreResponse>('/stores/' + router.query.store, {
-                params: {
-                    include: [
-                        'profile.state',
-                        'profile.district',
+            const storeResponse = await axios.get<StoreResponse>(
+                '/stores/' + router.query.store,
+                {
+                    params: {
+                        include: [
+                            'profile.state',
+                            'profile.district',
 
-                        'operators.user',
-                        'operators.user.profile.state',
-                        'operators.user.profile.district'
-                    ]
-                }
-            })
+                            'operators.user',
+                            'operators.user.profile.state',
+                            'operators.user.profile.district',
+                        ],
+                    },
+                },
+            )
 
             setStore(storeResponse.data)
             setStoreName(storeResponse.data.data.name)
@@ -83,12 +115,15 @@ const EditStore = () => {
             setPincode(storeResponse.data.data?.profile?.pincode + '')
             setState(storeResponse.data.data.profile?.state.id + '')
             setDistrict(storeResponse.data.data.profile?.district.id + '')
+            setPhone(storeResponse.data.data.profile?.emergency_contact || 0)
         })()
     }, [])
 
     useEffect(() => {
         const fetchDistricts = async () => {
-            const response = await axios.get<DistrictsResponse>('/states/' + state)
+            const response = await axios.get<DistrictsResponse>(
+                '/states/' + state,
+            )
             setDistricts(response.data)
         }
 
@@ -101,17 +136,21 @@ const EditStore = () => {
         setLoading(true)
 
         try {
-            const storeEditResponse = await axios.patch<BackendGeneralResponse>('stores/' + store?.data.id, {
-                operators: storeOperators,
-                name: storeName,
-                code: code,
-                profile: {
-                    address,
-                    pincode,
-                    state,
-                    district,
-                }
-            })
+            const storeEditResponse = await axios.patch<BackendGeneralResponse>(
+                'stores/' + store?.data.id,
+                {
+                    operators: storeOperators,
+                    name: storeName,
+                    code: code,
+                    profile: {
+                        address,
+                        pincode,
+                        state,
+                        district,
+                        emergency_contact: phone,
+                    },
+                },
+            )
 
             alert(storeEditResponse.data.message)
             router.reload()
@@ -127,12 +166,19 @@ const EditStore = () => {
                     <Text>Operators</Text>
                     <MultiSelect
                         onValueChange={setStoreOperators}
-                        defaultValue={currentStoreOps}>
-                        {operators != undefined ? operators.data.map(theOperator => (
-                            <MultiSelectItem value={theOperator.id + ''} key={theOperator.id}>
-                                {theOperator.name} - {theOperator.email} - {theOperator.phone}
-                            </MultiSelectItem>
-                        )) : (
+                        defaultValue={currentStoreOps}
+                    >
+                        {operators != undefined ? (
+                            operators.data.map((theOperator) => (
+                                <MultiSelectItem
+                                    value={theOperator.id + ''}
+                                    key={theOperator.id}
+                                >
+                                    {theOperator.name} - {theOperator.email} -{' '}
+                                    {theOperator.phone}
+                                </MultiSelectItem>
+                            ))
+                        ) : (
                             <MultiSelectItem value="">
                                 No free operators...
                             </MultiSelectItem>
@@ -149,56 +195,79 @@ const EditStore = () => {
                 <Text>Store Name</Text>
                 <TextInput
                     value={storeName}
-                    onInput={e => setStoreName(e.currentTarget.value)}
+                    onInput={(e) => setStoreName(e.currentTarget.value)}
                     error={errors?.errors.name != undefined}
                     errorMessage="Store name is required"
-                    className="mt-2" />
+                    className="mt-2"
+                />
+            </div>
+
+            <div className="mt-4">
+                <Text>Store Phone</Text>
+                <NumberInput
+                    value={phone}
+                    onValueChange={setPhone}
+                    error={errors?.errors.name != undefined}
+                    errorMessage="Store phone is required"
+                    className="mt-2"
+                />
             </div>
 
             <div className="mt-4">
                 <Text>Store Address</Text>
                 <TextInput
                     value={address}
-                    onInput={e => setAddress(e.currentTarget.value)}
-                    error={errors?.errors["profile.address"] != undefined}
+                    onInput={(e) => setAddress(e.currentTarget.value)}
+                    error={errors?.errors['profile.address'] != undefined}
                     errorMessage="Store address is required"
-                    className="mt-2" />
+                    className="mt-2"
+                />
             </div>
 
             <div className="mt-4">
                 <Text>Store custom code</Text>
                 <TextInput
                     value={code}
-                    onInput={e => setCode(e.currentTarget.value)}
+                    onInput={(e) => setCode(e.currentTarget.value)}
                     error={errors?.errors.custom_code != undefined}
                     errorMessage="Custom code for store is required"
-                    className="mt-2" />
+                    className="mt-2"
+                />
             </div>
 
             <div className="mt-4">
                 <Text>Store Pincode</Text>
                 <TextInput
                     value={pincode}
-                    onInput={e => setPincode(e.currentTarget.value)}
-                    error={errors?.errors["profile.pincode"] != undefined}
+                    onInput={(e) => setPincode(e.currentTarget.value)}
+                    error={errors?.errors['profile.pincode'] != undefined}
                     errorMessage="Store pincode is required"
-                    className="mt-2" />
+                    className="mt-2"
+                />
             </div>
 
-            {(states != undefined && store != undefined) && (
+            {states != undefined && store != undefined && (
                 <div className="mt-4">
                     <Text>Store State</Text>
                     <SearchSelect
                         defaultValue={store?.data?.profile?.state.id + ''}
-                        className="mt-2" onValueChange={value => setState(value)}>
-                        {states.data.map(theState => (
-                            <SearchSelectItem value={theState.id + ''} key={theState.id} icon={GlobeAsiaAustraliaIcon}>
+                        className="mt-2"
+                        onValueChange={(value) => setState(value)}
+                    >
+                        {states.data.map((theState) => (
+                            <SearchSelectItem
+                                value={theState.id + ''}
+                                key={theState.id}
+                                icon={GlobeAsiaAustraliaIcon}
+                            >
                                 {theState.name}
                             </SearchSelectItem>
                         ))}
                     </SearchSelect>
                     {errors?.errors['profile.state'] && (
-                        <Text color="red" className="mt-1">Please select a state</Text>
+                        <Text color="red" className="mt-1">
+                            Please select a state
+                        </Text>
                     )}
                 </div>
             )}
@@ -208,19 +277,28 @@ const EditStore = () => {
                 <SearchSelect
                     defaultValue={store?.data?.profile?.district.id + ''}
                     className="mt-2"
-                    onValueChange={value => setDistrict(value)}>
-                    {districts != undefined ? districts.data.districts.map(theDistrict => (
-                        <SearchSelectItem value={theDistrict.id + ''} key={theDistrict.id} icon={GlobeAsiaAustraliaIcon}>
-                            {theDistrict.name}
-                        </SearchSelectItem>
-                    )) : (
+                    onValueChange={(value) => setDistrict(value)}
+                >
+                    {districts != undefined ? (
+                        districts.data.districts.map((theDistrict) => (
+                            <SearchSelectItem
+                                value={theDistrict.id + ''}
+                                key={theDistrict.id}
+                                icon={GlobeAsiaAustraliaIcon}
+                            >
+                                {theDistrict.name}
+                            </SearchSelectItem>
+                        ))
+                    ) : (
                         <SearchSelectItem value="" icon={ArrowPathIcon}>
                             Loading...
                         </SearchSelectItem>
                     )}
                 </SearchSelect>
                 {errors?.errors['profile.district'] && (
-                    <Text color="red" className="mt-1">Please select a district</Text>
+                    <Text color="red" className="mt-1">
+                        Please select a district
+                    </Text>
                 )}
             </div>
 
@@ -232,7 +310,9 @@ const EditStore = () => {
                     loading={loading}
                     loadingText="Editing store..."
                     onClick={handleStoreEdit}
-                >Edit store</Button>
+                >
+                    Edit store
+                </Button>
             </Flex>
         </>
     )
